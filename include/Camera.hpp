dfc484342716befcc9f6e08f7ac83aa66e38d31f
songrr <1364117098@qq.com>
2023-01-05 16:43:42 +0800

    update

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -4,6 +4,7 @@
 #include "MvCameraControl.h"
 #include "ros/ros.h"
 
+using namespace std;
 namespace camera
 {
     cv::Mat frame;
@@ -31,13 +32,13 @@ namespace camera
         nRet = MV_CC_EnumDevices(MV_GIGE_DEVICE | MV_USB_DEVICE, &stDeviceList);
         if (MV_OK != nRet)
         {
-            printf("MV_CC_EnumDevices fail");
+            cout << "MV_CC_EnumDevices fail" << endl;
         }
         if (stDeviceList.nDeviceNum > 0)
         {
             for (int i = 0; i < stDeviceList.nDeviceNum; i++)
             {
-                printf("[device %d]:\n", i);
+                cout << "device " << i << endl;
                 MV_CC_DEVICE_INFO *pDeviceInfo = stDeviceList.pDeviceInfo[i];
                 if (pDeviceInfo == NULL)
                 {
@@ -47,35 +48,36 @@ namespace camera
         }
         else
         {
-            printf("find no device\n");
+            cout << "can not find device" <<endl;
         }
         nRet = MV_CC_CreateHandle(&handle, stDeviceList.pDeviceInfo[0]);
         if (MV_OK != nRet)
         {
-            printf("create camera error\n");
+            cout << "create handel failed" <<endl;
         }
         nRet = MV_CC_OpenDevice(handle);
         if (MV_OK != nRet)
         {
-            printf("open error\n");
+            cout << "open device error" <<endl;
         }
         nRet = MV_CC_SetEnumValue(handle,"TriggerMode",0);
         if (MV_OK == nRet){
-            printf("set tirgger \n");
+            cout << "success set trigger" <<endl;
         }
-        printf("start gtapping\n");
+        cout << "start grapping" <<endl;
         nRet = MV_CC_StartGrabbing(handle);
         if (MV_OK != nRet){
-            printf("grabbing failed\n");
+            cout << "grabbing error" <<endl;
         }
         nRet = pthread_mutex_init(&mutex,NULL);
         if (nRet != 0)
         {
-            perror("pthread_create failed\n");
+            cout << "thread create error"<<endl;
             exit(-1);
         }
         nRet = pthread_create(&threadID,NULL,workthread,handle);
-        printf("success thread\n");
+        cout << "pthread" <<nRet<<endl;
+
         
     }
     Camera::~Camera() {
@@ -86,24 +88,37 @@ namespace camera
         nRet = MV_CC_DestroyHandle(handle);
         pthread_mutex_destroy(&mutex);
     }
-    static void* Camera::workthread(void* pUser) {
-        int nRet;
+    void* Camera::workthread(void* pUser) 
+    {
+        int nRet = 1;
         int empty_frame = 0;
         unsigned char*  pDataForRGB = NULL;
+
         MVCC_INTVALUE stParam;
-        nRet = MV_CC_GetIntValue(pUser,"PayloadSize",&stParam);
         memset(&stParam,0,sizeof(MVCC_INTVALUE));
+        nRet = MV_CC_GetIntValue(pUser,"PayloadSize",&stParam);
+
         MV_FRAME_OUT_INFO_EX stImageInfo = {0};
-        MV_CC_PIXEL_CONVERT_PARAM stConvertParam = {0};
         memset(&stImageInfo,0,sizeof(MV_FRAME_OUT_INFO_EX));
+
+        MV_CC_PIXEL_CONVERT_PARAM stConvertParam = {0};
         unsigned char * pData = (unsigned char *)malloc(sizeof(unsigned char) * stParam.nCurValue);
-        if (NULL == pData){
-        printf("pData is NULL\n");
-        return NULL;
-        }   
         unsigned int nDataSize = stParam.nCurValue;
+        if (NULL == pData){
+            cout << "pData is None" <<endl;
+            return NULL;
+        }   
+        double start;
+       
         while(ros::ok()){
+            // nRet = MV_CC_SetCommandValue(pUser,"TriggerSoftware");
+        //     if (MV_OK != nRet){
+        //         cout << "set error"<< nRet <<endl;
+        //         break;
+        // }
+            start = static_cast<double>(cv::getTickCount());
             nRet = MV_CC_GetOneFrameTimeout(pUser,pData,nDataSize,&stImageInfo,15);
+            
             if( nRet != MV_OK){
                 if(++empty_frame > 100){
                     ROS_INFO("There are more than 100+ empty frame\n");
@@ -123,10 +138,26 @@ namespace camera
             nRet = MV_CC_ConvertPixelType(pUser,&stConvertParam);
             pthread_mutex_lock(&mutex);
             camera::frame = cv::Mat(stImageInfo.nHeight,stImageInfo.nWidth,CV_8UC3,pDataForRGB).clone();
-            cv::cvtColor(camera::frame,camera::frame,cv::COLOR_BGR2RGB);
+            frame_empty = 0;
+            // cv::cvtColor(camera::frame,camera::frame,cv::COLOR_BGR2RGB);
             pthread_mutex_unlock(&mutex);
-            cv::imshow("test",frame);
+            double time = ((double)cv::getTickCount() - start) / cv::getTickFrequency();
+            cout << "FPS:" << 1 / time << endl;
         }
+        free(pDataForRGB);
+        free(pData);
+
+    }  
+    void Camera::ReadImg(cv::Mat &img){
+        pthread_mutex_lock(&mutex);
+        if(frame_empty){
+            img = cv::Mat();
+        }else{
+            img = camera::frame;
+            frame_empty = 1;
+            
+        }
+        pthread_mutex_unlock(&mutex);
 
     }
 
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,2 +1,47 @@
-#include "Camera.h"
+#include "Camera.hpp"
+#include <iostream>
+#include <vector>
+#include <ros/ros.h>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
 
+using namespace std;
+
+
+
+int main(int argc,char *argv[]){
+    cv::Mat src;
+    ros::init(argc,argv,"hik_cam");
+    ros::NodeHandle nh;
+    camera::Camera MVS_cap(nh); 
+    image_transport::ImageTransport cam_image(nh);
+    image_transport::CameraPublisher img_pub = cam_image.advertiseCamera("/hik_cam/image",1000);
+    sensor_msgs::Image image_msg;
+    sensor_msgs::CameraInfo camera_info_msg;    
+    cv_bridge::CvImagePtr cv_ptr = boost::make_shared<cv_bridge::CvImage>();
+    cv_ptr->encoding = sensor_msgs::image_encodings::RGB8;
+    ros::Rate loop_rate(1);
+    
+    while(nh.ok()){
+        loop_rate.sleep();
+        ros::spinOnce();
+        MVS_cap.ReadImg(src);
+        if (src.empty())
+        {
+            cout << "empty"<< endl;
+            continue;
+        }
+        cv_ptr->image = src;
+        image_msg = *(cv_ptr->toImageMsg());
+        image_msg.header.stamp = ros::Time::now();
+        image_msg.header.frame_id = "hik_cam";
+        camera_info_msg.header.frame_id = image_msg.header.frame_id;
+	    camera_info_msg.header.stamp = image_msg.header.stamp;
+        img_pub.publish(image_msg, camera_info_msg);
+        
+    }
+
+
+    return 0;
+}
